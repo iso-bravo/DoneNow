04e923eb9592ba2b0d66c70f3d9bec37dc65acfa
iso-bravo <isolettebravo@gmail.com>
2024-05-29 10:26:02 -0700

    User repository

--- server/src/main/java/mx/edu/cetys/slip/repository/UserRepository.java
+++ server/src/main/java/mx/edu/cetys/slip/repository/UserRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    Optional<User> findByUsernameAndPassword(String username, String password);
 }
